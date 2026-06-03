60710dd731a0b93f32736ba97684a2f07a394cca
carlossilva23 <carlossilva23@arizona.edu>
2026-06-02 21:38:27 -0700

    Turned fetch_data.py user-friendly.

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,17 +1,36 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-ticker_list = ['IBM']
-ticker = yf.download(ticker_list, period = '1y')
-new = ticker['Close']
-#print(new)
-new.plot(kind='line')
-#plt.show()
+# Statement to catch date of highest closeing price.
+# max_date = new.idxmax()
 
-length = len(new)
-max = new.max()
-min = new.min()
-avg = new.mean()
-print(length, max, min, avg)
-max_date = new.idxmax()
-print(max_date)
+def fetch_stock(ticker_list): 
+    plt.figure()
+    for ticker in ticker_list:
+        portfolio = yf.download(ticker, period='1y')
+        if portfolio.empty:
+            # Debugging Statement
+            # print(portfolio.empty)
+            print(f"'{ticker}' does not exist.\n")    
+            exit()
+        else:
+            close_port = portfolio['Close']
+            plt.plot(close_port, label=f"{ticker}")
+    plt.legend()
+    plt.title("Closing Price Past Year")
+    plt.show()
+    
+
+
+def main(): 
+    ticker_list = []
+    while True:
+        ticker = input("Please type in a stock: ")
+        if ticker == "":
+            break
+        else: 
+            ticker_list.append(ticker.upper())
+    fetch_stock(ticker_list)
+
+main()
+
